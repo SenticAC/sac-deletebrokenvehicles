bc432fdb0267c335cfd2ce0b4a4cf38b7b44ba1a
SenticAC <111222332+SenticAC@users.noreply.github.com>
2022-12-31 02:47:23 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,12 @@
+Citizen.CreateThread(function()
+    while true do 
+        Citizen.Wait(10)
+		local vehicles = GetGamePool('CVehicle')
+        for k, v in pairs(vehicles) do 
+            if GetEntityHealth(v) == 0 then
+                SetEntityAsMissionEntity(v, false, false) 
+                DeleteEntity(v)
+            end
+		end
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Otis#6965'
+description 'Otis | Delete Broken Cars'
+version '1.0.0'
+
+client_script 'client.lua'
