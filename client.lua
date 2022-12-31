e325bd4270bbcee6d87e24da75067e6ab5c4c4a7
SenticAC <111222332+SenticAC@users.noreply.github.com>
2022-12-31 02:50:02 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,8 @@
 Citizen.CreateThread(function()
     while true do 
         Citizen.Wait(10)
-		local vehicles = GetGamePool('CVehicle')
+	local vehicles = GetGamePool('CVehicle')
+			
         for k, v in pairs(vehicles) do 
             if GetEntityHealth(v) == 0 then
                 SetEntityAsMissionEntity(v, false, false) 
